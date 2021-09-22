520fa9073199c2ab1a0e1e54c8bf028007534a89
Xander <xander.petit@student.howest.be>
2021-09-22 08:59:16 +0200

    remote desktop, ip en hostname

---
+++ opdracht 1/enable remote desktop.ps1
@@ -0,0 +1,5 @@
+﻿
+# Enable remote desktop
+
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
