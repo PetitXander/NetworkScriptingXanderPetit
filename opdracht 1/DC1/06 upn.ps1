83775d5b4694bc760b9c3a899cf51de5d1de76e6
Xander <xander.petit@student.howest.be>
2021-09-22 12:21:38 +0200

    6

---
+++ opdracht 1/DC1/06 upn.ps1
@@ -0,0 +1,5 @@
+﻿Get-ADForest | Set-ADForest -UPNSuffixes @{add="mijnschool.be"}
+
+$Suffix =  "mijnschool.be"
+$LocalUsers = Get-ADUser -Filter {UserPrincipalName -like '*intranet.mijnschool.be'} -Properties UserPrincipalName -ResultSetSize $nul
+$LocalUsers | foreach {$newUpn = $_.UserPrincipalName.Replace("intranet.mijnschool.be",$Suffix); $_ | Set-ADUser -UserPrincipalName $newUpn}
