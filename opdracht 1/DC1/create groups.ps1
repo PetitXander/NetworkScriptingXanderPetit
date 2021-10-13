54d07325bc9006a77e54b73ef0d1ef7356a2cfe5
Xander <xander.petit@student.howest.be>
2021-10-13 09:46:46 +0200

    DC1 Create groups

---
+++ opdracht 1/DC1/create groups.ps1
@@ -0,0 +1,20 @@
+﻿$groups = Import-Csv C:\Users\Administrator\Downloads\intranet.mijnschool.be\Groups.csv -Delimiter ";"
+
+
+ForEach ($group In $groups)
+{
+    $Path = $group.Path
+    $Name = $group.Name
+    $Description = $group.Description
+    $Displayname = $group.DisplayName
+    $GroupCategory = $group.GroupCategory
+    $GroupScope = $group.GroupScope
+
+
+    Write-Host $Name
+    Write-Host $Path
+    Write-Host $Displayname
+
+    New-ADGroup -Name $Name -Path $Path -DisplayName $Displayname -GroupCategory $GroupCategory -GroupScope $GroupScope -Description $Description -Verbose
+
+}
