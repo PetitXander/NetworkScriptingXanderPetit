c4abbb42af68a1672905818dc2f9fe2429c7db8b
Xander <xander.petit@student.howest.be>
2021-10-13 09:36:30 +0200

    create ou

---
+++ opdracht 1/DC1/create ou.ps1
@@ -0,0 +1,21 @@
+﻿
+
+
+
+$OUs = Import-Csv C:\Users\Administrator\Downloads\intranet.mijnschool.be\OUs.csv -Delimiter ";"
+
+
+ForEach ($OU In $OUs)
+{
+    $Name = $OU.Name
+    $Path = $OU.Path
+    $Description = $OU.Description
+    $Displayname = $OU."Display Name"
+
+    Write-Host $Name
+    Write-Host $Path
+    Write-Host $Displayname
+
+    New-ADOrganizationalUnit -Name $Name -Path $Path -DisplayName $Displayname -ProtectedFromAccidentalDeletion $False -Description $Description -Server "win07-DC1.intranet.mijnschool.be" -Verbose
+
+}
