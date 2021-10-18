f1da615e499bec97c62b37786d9df43a0f5306dc
Xander <xander.petit@student.howest.be>
2021-10-18 14:55:56 +0200

    groupmemebers

---
+++ opdracht 1/DC1/11 GroupMembers.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+$GroupMembers = Import-Csv C:\Users\Administrator\Downloads\intranet.mijnschool.be\GroupMembers.csv -Delimiter ";"
+
+ForEach($Mem In $GroupMembers)
+{
+    $Member = $Mem.Member
+    $Identity = $Mem.Identity
+
+    Write-Host "Adding $Member to $Identity" -ForegroundColor Green
+
+    $CreateGroup = Add-ADGroupMember `
+        -Identity $Identity `
+        -Members $Member
+}
