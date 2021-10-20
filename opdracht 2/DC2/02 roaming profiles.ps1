83b19506684ef2c3ab2151e22d7cd5abbba4e2e0
Xander <xander.petit@student.howest.be>
2021-10-20 10:48:58 +0200

    roaming profile

---
+++ opdracht 2/DC2/02 roaming profiles.ps1
@@ -0,0 +1,6 @@
+﻿$users = Get-ADGroupMember -Identity Secretariaat
+
+foreach ($user in $users) {
+$name = $user.SamAccountName
+Set-ADUser -Identity $user -Profilepath "\\Win07-DC2\Profiles$\$name"
+}
