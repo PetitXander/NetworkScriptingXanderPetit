bdf65b7177d016d3c77f14211b5f1f0610f3ad71
xander petit <xander.petit@student.howest.be>
2021-12-01 15:47:09 +0100

    fixed typos

--- Bash/4.Alias_Record.sh
+++ Bash/4.Alias_Record.sh
@@ -19,12 +19,12 @@ apt install apache2 bind9 bind9utils wget -y
 wget --user=ftp --password=ftp ftp://ftp.rs.internic.net/domain/db.cache -O /etc/bind/db.root
 
 # Adjusting /etc/default/bind9
-echo "OPTIONS='-4 -u bind'" >> /etc/default/bind9
+echo "OPTIONS=\"-4 -u bind\"" >> /etc/default/bind9
 
 # Adjusting /etc/bind/named.conf.local
-printf "zone ' mctinternal.be' {\n type master;\nfile '/etc/1bind/zones/ mctinternal.be';\n};\n\n" >> /etc/bind/named.conf.local
+printf "zone \" mctinternal.be\" {\n type master;\nfile \"/etc/1bind/zones/nmctinternal.be\";\n};\n\n" >> /etc/bind/named.conf.local
 
-printf "zone '214.168.192.in-addr.arpa' {\ntype master;\n file '/etc/bind/zones/reverse/rev.X.168.192';\n};\n\n" >> /etc/bind/named.conf.local
+printf "zone \"214.168.192.in-addr.arpa\" {\ntype master;\n file \"/etc/bind/zones/reverse/rev.214.168.192\";\n};\n\n" >> /etc/bind/named.conf.local
 
 # Creating directories
 echo "Creating directories that are needed for bind9 reverse zones"
@@ -36,7 +36,7 @@ echo "Adjusting nmctinternal.be"
 echo ";\n" >> /etc/bind/zones/nmctinternal.be
 echo "; BIND data for mctinternal.be\n" >> /etc/bind/zones/nmctinternal.be
 echo ";\n" >> /etc/bind/zones/nmctinternal.be
-echo "$TTL 3h\n" >> /etc/bind/zones/nmctinternal.be
+echo "\$TTL 3h\n" >> /etc/bind/zones/nmctinternal.be
 echo "@       IN      SOA     ns1. mctinternal.be. admin. mctinternal.be. (\n" >> /etc/bind/zones/nmctinternal.be
 echo "                        1       ; serial\n" >> /etc/bind/zones/nmctinternal.be
 echo "                        3h      ; refresh\n" >> /etc/bind/zones/nmctinternal.be
@@ -49,12 +49,12 @@ echo "@       IN      NS      ns2. mctinternal.be.\n" >> /etc/bind/zones/nmctint
 echo "\n" >> /etc/bind/zones/nmctinternal.be
 echo "www                     IN      CNAME   mctinternal.be.\n" >> /etc/bind/zones/nmctinternal.be
 
-touch /etc/bind/zones/reverse/214.168.192.in-addr.arpa
+touch /etc/bind/zones/reverse/rev.214.168.192.in-addr.arpa
 echo "Adjusting reverse lookup zone"
 echo ";\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo "; BIND reverse file for 214.168.192.in-addr.arpa\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo ";\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
-echo "$TTL    604800\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
+echo "\$TTL    604800\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo "@       IN      SOA     ns1.mctinternal.be. admin.mctinternal.be. (\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo "                                1       ; serial\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo "                                3h      ; refresh\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
@@ -63,4 +63,6 @@ echo "                                1w      ; expire\n" >> /etc/bind/zones/rev
 echo "                                1h )    ; minimum\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 echo ";\n" >> /etc/bind/zones/reverse/214.168.192.in-addr.arpa
 
+named-checkconf
+systemctl restart bind9
 echo "Done!!"
